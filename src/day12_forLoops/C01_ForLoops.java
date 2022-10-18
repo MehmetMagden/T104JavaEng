f7f1685bf01e093e1c356950a169209890b33ebe
Mehmet Maden <yasamtr@gmail.com>
2022-10-18 17:50:39 +0300

    15.10.22

---
+++ src/day12_forLoops/C01_ForLoops.java
@@ -0,0 +1,28 @@
+package day12_forLoops;
+
+public class C01_ForLoops {
+
+    public static void main(String[] args) {
+
+        String str ="Java\n";
+        System.out.println(str.repeat(10));
+
+        System.out.println("Java");
+        System.out.println("Java");
+        System.out.println("Java");
+        System.out.println("Java");
+        System.out.println("Java");
+        System.out.println("Java");
+        System.out.println("Java");
+        System.out.println("Java");
+        System.out.println("Java");
+        System.out.println("Java");
+
+        // 1 to 100
+        // 3 >> increase number by 7 >> until 100
+
+
+    }
+
+
+}
