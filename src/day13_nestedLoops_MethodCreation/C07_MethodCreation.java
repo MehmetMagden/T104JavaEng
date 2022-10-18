92c864243201a2cccb11545f445aae6f612d8aff
Mehmet Maden <yasamtr@gmail.com>
2022-10-18 22:49:23 +0300

    todays secodn

---
+++ src/day13_nestedLoops_MethodCreation/C07_MethodCreation.java
@@ -0,0 +1,20 @@
+package day13_nestedLoops_MethodCreation;
+
+public class C07_MethodCreation {
+
+    // lets do a+b in a method
+
+
+    public static void main(String[] args) {
+
+        addition(15,20);
+
+    }
+
+    public static void addition (int a, int b){
+
+        System.out.println(a+b);
+
+    }
+
+}
