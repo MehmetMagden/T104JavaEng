592275f849fcc044b618145ba07e12a9cfd4a333
Mehmet Maden <yasamtr@gmail.com>
2022-12-02 19:02:32 +0300

    Collections

---
+++ src/day42_collectionsDeque/C02_DequeQuestions.java
@@ -0,0 +1,41 @@
+package day42_collectionsDeque;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+public class C02_DequeQuestions {
+
+    public static void main(String[] args) {
+
+        // create a deque and add Y at the begining of all elements
+
+        Deque<String> dequeList= new LinkedList<>();
+        dequeList.add("Nutella");
+        dequeList.add("Cheese");
+        dequeList.add("Speaker");
+
+        Deque<String> tempList = new LinkedList<>();
+        String product ="";
+
+        while (true){
+
+            try {
+                product= dequeList.remove();
+                tempList.add("Y"+product);
+            } catch (Exception e) {
+                break;
+            }
+
+        }
+        dequeList=tempList;
+        System.out.println(dequeList);
+
+
+
+
+
+
+    }
+
+}
