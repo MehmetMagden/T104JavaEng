87f7a60b14f204d1e76b71d4c2848d3ad5f5d17a
Mehmet Maden <yasamtr@gmail.com>
2022-12-09 19:55:15 +0300

    little update

--- src/Projects/P03_StorageProgram.java
+++ src/Projects/P03_StorageProgram.java
@@ -8,44 +8,46 @@ public class P03_StorageProgram {
      *
      * It is a warehouse application, which is a system implemented in a working factory.
      *
-     * 1-) First of all, the product is defined here. id productName manufacturer quantity unit and shelf (6 fields available)
+     * 1-) First of all, the product should be defined here. id productName manufacturer quantity unit and shelf (6 fields available)
      *
      * 2-) We will create methods.
-     * Product Description ==> The name, manufacturer and unit of the product will be entered. id will be created.
+     * productDescription ==> The name, manufacturer and unit of the product will be entered. id will be created.
      *
-     * ProductList ==> defined products will be listed. If the product quantity and shelf number are not defined, the default value will appear.
+     * productList ==> defined products will be listed. If the product quantity and shelf number are not defined, the default value will appear.
      *
-     * Product Entry ==> we will enter with the id number of the product we want to log in.
+     * productEntry ==> we will enter a product with the id number ( or system can create an id number )
      *
-     * Put the product on the shelf ==> we will choose the product from the list and put the product on the shelf according to the id number.
+     * putProductToShelf ==> we will choose the product from the list and put the product on the shelf according to the id number.
      *
-     * product Output ==> we will select the product from the list and we will check out the product. Only quantity changes will be made from the product list here.
+     * removeProduct ==> we will select the product from the list and we will check out the product. Only quantity changes will be made from the product list here.
      * The number of products cannot be less than 0. When 0, the product description should not be deleted. just let the amount be 0.
      * ===> all the changes we have made will also appear in the list.
      *
      *
-         id      name    provider       quantity    unit            shelf
-     *------------------------------------------------ --------------
-     *  1000    Flour    Hekimoglu       0          bags            null
+         id      name    manufacturer       quantity    unit            shelf
+     *------------------------------------------------ --------------------------
+     *  1000    flour    Hekimoglu              0        bags            null
      *
-     * product entry ==> id:1000 product entry :5
+     * productEntry ==> id:1000 product entry :5
      * productList ==>
      *
-     * id name generator quantity unit rack
+     * id   name    manufacturer    quantity    unit    shelf
      *------------------------------------------------ --------------
-     * or: 5 bags of 1000 flour physicians null
+     * 1000 flour   hekimolgu       5         bag     null
      *
-     * Put productRafa ==> id:1000 shelf : shelf1
+     * putproductToShelf ==> id:1000 shelf : shelf1
      * productList ==>
-     * id name generator quantity unit rack
-     *------------------------------------------------ --------------
-     * or: 5 sacks of 1000 flour with Hekimoglu Rack1
      *
-     * product output ==> id 1000: product output : 3 (remaining 2 sacks of flour)
-     * productList ==>
-     * id name generator quantity unit rack
+     * id   name    manufacturer    quantity    unit    shelf
      *------------------------------------------------ --------------
-     * or: 2 bags of 1000 flour with Hekimoglu Rack1
+     * 1000 flour   hekimolgu       5           bag     shelf1
+     *
+     * product output ==> id 1000: removeProduct : 3 (remaining 2 sacks of flour)
+     * productList ==>
+     *
+     * id   name    manufacturer    quantity    unit    shelf
+     *------------------------------------------------ --------------
+     * 1000 flour   hekimolgu       2           bag     shelf1
      *
      *
      */
